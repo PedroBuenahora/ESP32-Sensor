d922f7b9afb5b10b79e976a107bf37f6f0983ffe
Peter <pedrobuenahora@hotmail.com>
2022-09-12 01:51:29 -0500

    Page edited minor fixes
    Minor changes in the style and changes made in the ESP32 code in order to send it correctly to the page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,6 +8,9 @@ const tempElement = document.getElementById("temp");
 const humElement = document.getElementById("hum");
 const presElement = document.getElementById("pres");
 const brigElement = document.getElementById("bri");
+const bulElement = document.getElementById("bul");
+const pumElement = document.getElementById("pum");
+const fanElement = document.getElementById("fan");
 
 // MANAGE LOGIN/LOGOUT UI
 const setupUI = (user) => {
@@ -27,13 +30,19 @@ const setupUI = (user) => {
     var dbPathTemp = 'UsersData/' + uid.toString() + '/temperature';
     var dbPathHum = 'UsersData/' + uid.toString() + '/humidity';
     var dbPathPres = 'UsersData/' + uid.toString() + '/pressure';
-    var dbPathBrig = 'UsersData/' + uid.toString() + '/brightness';;
+    var dbPathBrig = 'UsersData/' + uid.toString() + '/brightness';
+    var dbPathBul = 'UsersData/' + uid.toString() + '/bulbStatus';
+    var dbPathPum = 'UsersData/' + uid.toString() + '/pumpStatus';
+    var dbPathFan = 'UsersData/' + uid.toString() + '/fanStatus';
 
     // Database references
     var dbRefTemp = firebase.database().ref().child(dbPathTemp);
     var dbRefHum = firebase.database().ref().child(dbPathHum);
     var dbRefPres = firebase.database().ref().child(dbPathPres);
-    var dbRefBrig = firebase.database().ref().child(dbPathBrig );
+    var dbRefBrig = firebase.database().ref().child(dbPathBrig);
+    var dbRefNBul = firebase.database().ref().child(dbPathBul);
+    var dbRefPum = firebase.database().ref().child(dbPathPum);
+    var dbRefFan = firebase.database().ref().child(dbPathFan);
 
     // Update page with new readings
     dbRefTemp.on('value', snap => {
@@ -50,6 +59,18 @@ const setupUI = (user) => {
 
     dbRefBrig.on('value', snap => {
       brigElement.innerText = snap.val().toFixed(2);
+    });
+
+    dbRefNBul.on('value', snap => {
+      bulElement.innerText = snap.val().toString();
+    });
+
+    dbRefPum.on('value', snap => {
+      pumElement.innerText = snap.val().toString();
+    });
+
+    dbRefFan.on('value', snap => {
+      fanElement.innerText = snap.val().toString();
     });
 
   // if user is logged out
